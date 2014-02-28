277ec8c0502bb21f9a9bd084dfd2ed02577a76fe
Melanie Archer <job@twobanjos.com>
2014-02-28 14:55:52 -0800

    Small test for random Weapon

--- tests.rb
+++ tests.rb
@@ -1,17 +1,13 @@
 
 ##
 # Tests
-session = Session.new
+# session = Session.new
 melanie = User.new
-session.add_user(melanie) # @users << melanie
-
-score = session.report_score # gets @score from Session
-melanie.score = score
-
 p melanie.id == 1
 p melanie.name == 4155551212
 
-weapon = Weapon.new(rand(0..4))
+weapon = Weapon.new(['rock', 'spock', 'paper', 'lizard', 'scissors'].sample)
+p weapon
 # cf. http://en.wikipedia.org/wiki/File:Normal_form_matrix_of_Rock-paper-scissors-lizard-Spock.jpg
 spock = Weapon.new('spock')
 p spock.name_to_number
