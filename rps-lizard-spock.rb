5c51839644014708b6927b2667c578420ef23a2d
jluyke <jordanluyke@gmail.com>
2014-02-28 15:30:26 -0800

    added session code

--- rps-lizard-spock.rb
+++ rps-lizard-spock.rb
@@ -1,15 +1,27 @@
 require_relative './user.rb'
 require_relative './weapon.rb'
- require_relative './tests.rb'
+require_relative './tests.rb'
+
+class Session #jordan
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    # @player1_score = 0
+    # @player2_score = 0
+  end
+
+  def start_game_loop(game_count)
+    game_count.times do |count|
+      game = Game.new(@player1, @player2)
+      game.play
+      p game.winner.name
+    end
+  end
+end
 
 class Game #jason
   #attributes: user weapon, computer weapon, the result
   #behaviors: user_choose_weapon, computer_choose_weapon, compute_result, resolve_tie, game_loop
-end
-
-class Session #jordan
-  #attributes: wins, losses, choices, user_id
-  #behaviors: start a session, input, update, render
 end
 
 #additional Twilio classes, database classes
