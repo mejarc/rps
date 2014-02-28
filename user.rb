88ed23bfcce59a3d4042e4bad97b4d49f07aacb3
Melanie Archer <job@twobanjos.com>
2014-02-28 15:22:00 -0800

    More user/weapon refactoring

--- user.rb
+++ user.rb
@@ -3,13 +3,19 @@ class User #melanie
   #attributes: id (database-generated primary key), name (default is user's phone #)
   #behavior: add_up_total_score
   attr_accessor :id, :name, :score
+  attr_reader :weapon
 
   def initialize(id = 1, name = 4155551212)
     @id = id
     @name = name
     @score = 0
+    # @weapons = Hash.
   end
 
+  def set_weapon(weapon_name)
+    @weapon = Weapon.new(weapon_name)
+  end
+  
   def add_up_score
     # needs to compute from Session?
   end
--- weapon.rb
+++ weapon.rb
@@ -2,13 +2,13 @@
 class Weapon #subclass for each weapon, or just have 5 instances, #melanie
   #attributes: name, (what_it_beats, what_it_loses_to) or who_beats_who(all)
   #readers
-
   def initialize(name)
     @name = name
+    @index = name_to_number(@name)
   end
 
-  def name_to_number
-    case @name
+  def name_to_number(name)
+    case name
     when 'rock'
       return 0
     when 'spock'
@@ -22,19 +22,21 @@ class Weapon #subclass for each weapon, or just have 5 instances, #melanie
     end
   end
 
-  def who_beats_who
-    player_weapon = name_to_number
-    # Game must generate `opponent`
-    # placeholder
-    opponent = 4
-    difference = (player_weapon - opponent) % 5
-    if [1, 2].include?(difference)
-      'Player wins'
-    elsif difference.zero?
-      'Computer wins'
-    else
-      'tie'
-    end
-  end
+# put in Game
+  # def who_beats_who
+  #   player_weapon = weapon.index
+  #   # Game must generate `opponent`
+  #   # placeholder
+  #   opponent = 4
+  #   difference = (player_weapon - opponent) % 5
+  #   if [1, 2].include?(difference)
+  #     'Player wins'
+  #   elsif difference.zero?
+  #     'Computer wins'
+  #   else
+  #     'tie'
+  #   end
+  # end
 end
+
 
