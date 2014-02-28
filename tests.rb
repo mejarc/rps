4de137df21f5f2c8f2d84cfe96092ce924d2023c
jluyke <jordanluyke@gmail.com>
2014-02-28 15:47:30 -0800

    update test.rb

--- tests.rb
+++ tests.rb
@@ -3,6 +3,8 @@
 # Tests
 # session = Session.new
 melanie = User.new
+computer = User.new
+
 p melanie.id == 1
 p melanie.name == 4155551212
 
@@ -11,3 +13,6 @@ p weapon
 # cf. http://en.wikipedia.org/wiki/File:Normal_form_matrix_of_Rock-paper-scissors-lizard-Spock.jpg
 spock = Weapon.new('spock')
 p spock.name_to_number('spock') == 1
+
+game_session = Session.new(melanie, computer)
+session.start_loop(5)
